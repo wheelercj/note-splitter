1c25fa29c42a29a5877f8c67b2e5944edc1ca21c
wheelercj <chrisw289@gmail.com>
2020-11-16 13:21:46 -0800

    Add support for .html files

--- check_media.py
+++ check_media.py
@@ -1,51 +1,55 @@
 # Search the zettelkasten folder for unused assets,
 # and let the user decide what to do with them.
 
-# To add a new asset type to the program,
+# To add support for a new asset type to the program,
 # change both the asset_types and asset_link_pattern variables.
 
 import os
 import sys
 import re
-import send2trash
+from send2trash import send2trash
 import datetime
 import pyautogui
 
 
 zettel_types = ('.md', '.markdown')
-asset_types = ('.jpg', '.jpeg', '.png', '.pdf', '.mp4')
-asset_link_pattern = r'(?<=(\(|\\|/))([^(\(|\\|/)]+)\.(jpg|jpeg|png|pdf|mp4)\)'
+asset_types = ('.jpg', '.jpeg', '.png', '.pdf', '.mp4', '.html')
+asset_link_pattern = r'(?<=(\(|\\|/))([^(\(|\\|/)]+)\.(jpg|jpeg|png|pdf|mp4|html)\)'
 
 
 def main():
-    unused_assets = find_unused_assets()
-    sorted_assets = dict()
+    try:
+        unused_assets = find_unused_assets()
+        sorted_assets = dict()
 
-    # Sort the assets by value.
-    for key, value in sorted(unused_assets.items(), key=by_value, reverse=True):
-        sorted_assets[key] = value
+        # Sort the assets by value.
+        for key, value in sorted(unused_assets.items(), key=by_value, reverse=True):
+            sorted_assets[key] = value
 
-    print_asset_info(sorted_assets)
-    total_bytes = sum(sorted_assets.values())
-    print(f'\nFound {len(unused_assets)} unused asset(s) taking {format_bytes(total_bytes)} of memory.')
+        print_asset_info(sorted_assets)
+        total_bytes = sum(sorted_assets.values())
+        print(f'\nFound {len(unused_assets)} unused asset(s) taking {format_bytes(total_bytes)} of memory.')
 
-    if len(sorted_assets) == 0:
-        return
+        if len(sorted_assets) == 0:
+            return
 
-    print('\nMenu:')
-    print('1. Manually validate unused assets.')
-    print('2. Send all unused assets to the recycle bin.')
-    print('3. Exit')
-    print('> ', end='', flush=True)
-    choice = input()
+        print('\nMenu:')
+        print('1. Manually validate unused assets.')
+        print('2. Send all unused assets to the recycle bin.')
+        print('3. Exit')
+        print('> ', end='', flush=True)
+        choice = input()
 
-    os.chdir('..')
-    if choice == '1':
-        validate_unused_assets(sorted_assets)
-    elif choice == '2':
-        delete_unused_assets(sorted_assets)
-    else:
-        sys.exit(0)
+        os.chdir('..')
+        if choice == '1':
+            validate_unused_assets(sorted_assets)
+        elif choice == '2':
+            delete_unused_assets(sorted_assets)
+        else:
+            sys.exit(0)
+
+    except SystemExit:
+        pass
 
 
 # For sorting a dictionary by value with the sorted() function.
@@ -64,10 +68,27 @@ def find_unused_assets():
         if asset_name not in asset_links:
             asset_path = '../' + asset_name
             unused_assets[asset_name] = os.path.getsize(asset_path)
+            # If the unused asset is an .html file, get the size of the corresponding folder too.
+            if asset_name.endswith('.html'):
+                unused_assets[asset_name] += get_size(asset_path[0:-5] + '_files')
 
     return unused_assets
 
 
+# Get the total memory size of an entire folder.
+def get_size(start_path='.'):
+    total_size = 0
+    for dirpath, _, filenames in os.walk(start_path):
+        for filename in filenames:
+            filepath = os.path.join(dirpath, filename)
+            # Skip the file if it is a symbolic link.
+            if not os.path.islink(filepath):
+                total_size += os.path.getsize(filepath)
+
+    return total_size
+
+
+# Returns lists of the names of all zettels and assets in the zettelkasten.
 def find_file_names():
     # Get all files in the current directory.
     file_names = os.listdir('..')
@@ -84,7 +105,7 @@ def find_file_names():
     return zettel_names, asset_names
 
 
-# Find the asset file links in the zettels.
+# Returns a list of all asset file links in the zettels.
 def find_asset_links(zettel_names):
     asset_links = []
 
@@ -116,7 +137,7 @@ def print_asset_info(unused_assets):
 
 
 # Convert bytes to kilobytes, megabytes, etc.
-# Returns a string of the converted bytes and the appropriate units.
+# Returns a string of the converted bytes with the appropriate units.
 def format_bytes(Bytes):
     power = 0
     while Bytes > 1024:
@@ -133,6 +154,8 @@ def format_bytes(Bytes):
         return result + ' GB'
     elif power == 12:
         return result + ' TB'
+    else:
+        raise ValueError('File size not yet supported.')
 
 
 # For each asset:
@@ -146,16 +169,34 @@ def validate_unused_assets(unused_assets):
         Bytes = format_bytes(value)
         print(f'{key}{Bytes:>10}')
 
-        os.startfile(key)  # Open the asset for the user to view.
+        # Open the asset for the user to view.
+        os.startfile(key)
         pyautogui.sleep(0.5)
         asset_window = pyautogui.getActiveWindow()
-        pyautogui.hotkey('alt', 'tab')  # Bring the program's window back to the front.
+        # Bring the program's window back to the front.
+        pyautogui.hotkey('alt', 'tab')
         choice = input('Send asset to the recycle bin? [y/n]: ')
-        asset_window.close()
+        # Close the asset window.
+        if key.endswith('.html'):
+            pyautogui.hotkey('alt', 'tab')
+            pyautogui.hotkey('ctrl', 'w')
+            pyautogui.hotkey('alt', 'tab')
+        else:
+            asset_window.close()
 
         # Respond to the user's choice.
         if choice == 'y':
-            send2trash.send2trash(key)
+            send2trash(key)
+            # If the asset is an .html file, move the corresponding folder.
+            if key.endswith('.html'):
+                folder_name = key[0:-5] + '_files'
+                if os.path.isdir(folder_name):
+                    try:
+                        send2trash(folder_name)
+                    except OSError:
+                        print('OSError')
+                else:
+                    print(f'Could not find folder \'{folder_name}\'.')
             print('Asset sent to the recycle bin.\n')
         elif choice == 'n':
             print('Asset saved.\n')
@@ -177,8 +218,7 @@ def delete_unused_assets(unused_assets):
 
     # Create a folder for all the files that will be sent to the recycle bin.
     # The folder's name will be the current datetime and this program's name.
-    date_and_time_with_colons = str(datetime.datetime.now())
-    date_and_time = date_and_time_with_colons.replace(':', '')
+    date_and_time = str(datetime.datetime.now()).replace(':', '').replace('.', '')
     new_folder_name = date_and_time + ' check_media'
     os.mkdir(new_folder_name)
 
@@ -186,14 +226,21 @@ def delete_unused_assets(unused_assets):
     for asset_name in unused_assets.keys():
         new_path = os.path.join(new_folder_name, asset_name)
         os.rename(asset_name, new_path)
+        # If the asset is an .html file, move the corresponding folder.
+        if asset_name.endswith('.html'):
+            asset_folder_name = asset_name[0:-5] + '_files'
+            new_path = new_path[0:-5] + '_files'
+            for retry in range(100):
+                try:
+                    os.rename(asset_folder_name, new_path)
+                    break
+                except OSError:
+                    pass
 
     # Move the new folder to the recycle bin.
-    send2trash.send2trash(new_folder_name)
+    send2trash(new_folder_name)
     print(f'\nAll unused assets sent to the recycle bin in the new folder {new_folder_name}\n')
 
 
 if __name__ == '__main__':
-    try:
-        main()
-    except SystemExit:
-        pass
+    main()
