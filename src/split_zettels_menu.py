74c6fdd7f87384a1d4a4a2c08eb481d31fbf451b
wheelercj <chrisw289@gmail.com>
2020-12-22 13:53:56 -0800

    Move generic listbox functions to new file

--- src/split_zettels_menu.py
+++ src/split_zettels_menu.py
@@ -1,12 +1,10 @@
 # Internal imports
 from split_zettels import *
-from common import get_zettel_links, show_file
+from zettel_listbox import *
+from common import get_zettel_links
 
 # External imports
-import os
 import PySimpleGUI as sg
-import webbrowser
-from tkinter.filedialog import askdirectory
 
 
 def split_zettels_menu():
@@ -114,44 +112,6 @@ def handle_results_menu_event(event, values, window, new_zettels, zettels_sans_h
         paths = get_listbox_paths(values['-z_sans_h-'], zettels_sans_h)
         handle_zettel_listbox_event(event, window, paths, zettels_sans_h)
         window['-z_sans_h-'].update(zettels_sans_h.links)
-
-
-def handle_zettel_listbox_event(event, window, paths, zettels):
-    if event.startswith('-open'):
-        for path in paths:
-            if os.path.exists(path):
-                webbrowser.open('file://' + path)
-            else:
-                sg.Popup(f'File not found: {path}')
-                zettels.remove_path(path)
-    elif event.startswith('-show'):
-        for path in paths:
-            if os.path.exists(path):
-                show_file(path)
-            else:
-                sg.Popup(f'File not found: {path}')
-                zettels.remove_path(path)
-    elif event.startswith('-move'):
-        destination = askdirectory(title='Select the destination folder.', mustexist=True)
-        if destination != '':
-            for path in paths:
-                new_path = os.path.join(destination, os.path.split(path)[-1])
-                os.rename(path, new_path)  # TODO: update any relative file links in the zettel.
-                zettels.repath(path, new_path)
-
-
-def get_listbox_paths(selected_links, listbox_zettels):
-    # Determine which links to get the paths of.
-    if len(selected_links) == 0:
-        return listbox_zettels.paths
-
-    # Convert the links to paths.
-    selected_paths = []
-    for link in selected_links:
-        path = listbox_zettels.path_of_link(link)
-        selected_paths.append(path)
-
-    return selected_paths
 
 
 if __name__ == '__main__':
---
+++ src/zettel_listbox.py
@@ -0,0 +1,46 @@
+# Internal imports
+from common import show_file
+
+# External imports
+import os
+import webbrowser
+import PySimpleGUI as sg
+from tkinter.filedialog import askdirectory
+
+
+def handle_zettel_listbox_event(event, window, paths, zettels):
+    if event.startswith('-open'):
+        for path in paths:
+            if os.path.exists(path):
+                webbrowser.open('file://' + path)
+            else:
+                sg.Popup(f'File not found: {path}')
+                zettels.remove_path(path)
+    elif event.startswith('-show'):
+        for path in paths:
+            if os.path.exists(path):
+                show_file(path)
+            else:
+                sg.Popup(f'File not found: {path}')
+                zettels.remove_path(path)
+    elif event.startswith('-move'):
+        destination = askdirectory(title='Select the destination folder.', mustexist=True)
+        if destination != '':
+            for path in paths:
+                new_path = os.path.join(destination, os.path.split(path)[-1])
+                os.rename(path, new_path)  # TODO: update any relative file links in the zettel.
+                zettels.repath(path, new_path)
+
+
+def get_listbox_paths(selected_links, listbox_zettels):
+    # Determine which links to get the paths of.
+    if len(selected_links) == 0:
+        return listbox_zettels.paths
+
+    # Convert the links to paths.
+    selected_paths = []
+    for link in selected_links:
+        path = listbox_zettels.path_of_link(link)
+        selected_paths.append(path)
+
+    return selected_paths
