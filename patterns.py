1ab1a48a8e76f7a9e0793e44441d5538f3ea5252
wheelercj <chrisw289@gmail.com>
2021-09-24 13:49:04 -0700

    Write a comment for each regular expression

--- patterns.py
+++ patterns.py
@@ -1,5 +1,5 @@
 # Resources for learning and using regex:
-# https://wheelercj.github.io/notes/20210506235005.html
+# https://wheelercj.github.io/notes/posts/20210506235005.html
 
 
 import re
@@ -7,5 +7,6 @@ import re
 
 class patterns:
     """Compiled regex patterns."""
-    any_header = re.compile(r'(^#+ .+)')
-    tags = re.compile(r'(.|\B)(#[a-zA-Z0-9_-]+)')
+    any_header = re.compile(r'(^#+ .+)')  # A markdown header of any level.
+    tags = re.compile(r'(.|\B)(#[a-zA-Z0-9_-]+)')  # For group[1] to be
+        # a tag, group[0] must be in ('', ' ', '\t').
