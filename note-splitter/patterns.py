53c9466f1888ff40d1c280145bd6912b17cf6f98
wheelercj <chrisw289@gmail.com>
2021-10-15 12:55:30 -0700

    Add more markdown patterns

--- note-splitter/patterns.py
+++ note-splitter/patterns.py
@@ -1,7 +1,7 @@
 r"""Compiled regular expressions.
 
 Resources for learning and using regex: 
-https://wheelercj.github.io/notes/posts/20210506235005.html
+https://wheelercj.github.io/notes/pages/20210506235005.html
 
 Attributes
 ----------
@@ -11,11 +11,23 @@ tags : re.Pattern
     The pattern for a tag and the character before the tag. For 
     :code:`group[1]` to be a tag, :code:`group[0]` must be in 
     :code:`('', ' ', '\t')`.
+horizontal_rule : re.Pattern
+    The pattern for a markdown-style horizontal rule, which is composed
+    of three or more minuses, underscores, or asterisks. There may be 
+    any number of whitespace characters anywhere on the line.
+frontmatter_fence : re.Pattern
+    The pattern for a frontmatter delimiter.
+code_fence : re.Pattern
+    The pattern for a multi-line code block delimiter; the line starts
+    with either three backticks or three tildes.
 """
 
 
 import re
 
 
-any_header = re.compile(r'(^#+ .+)')
+any_header = re.compile(r'^#+ .+')
 tags = re.compile(r'(.|\B)(#[a-zA-Z0-9_-]+)')
+horizontal_rule = re.compile(r'\s*(?:(?:-\s*){3,}|(?:\*\s*){3,}|(?:_\s*){3,})')
+frontmatter_fence = re.compile(r'^---$')
+code_fence = re.compile(r'^(?:```|~~~).*')
