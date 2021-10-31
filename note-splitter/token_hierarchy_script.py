9ada62b0303c8f09ec99186ba0264de51c97c99b
wheelercj <chrisw289@gmail.com>
2021-10-31 12:34:36 -0700

    Clarify how token_hierarchy_script works

--- note-splitter/token_hierarchy_script.py
+++ note-splitter/token_hierarchy_script.py
@@ -14,6 +14,7 @@ import tokens
 
 
 def main():
+    """Detects, creates, and saves the token hierarchy to a file."""
     token_hierarchy: str = create_token_hierarchy()
     token_hierarchy_path = 'docs/token-hierarchy.rst'
     try:
@@ -22,10 +23,11 @@ def main():
     except FileNotFoundError:
         with open('../' + token_hierarchy_path, 'w') as file:
             file.write(token_hierarchy)
-    print('token hierarchy saved to docs/token-hierarchy.rst')
+    print('Token hierarchy saved to docs/token-hierarchy.rst')
 
 
 def create_token_hierarchy() -> str:
+    """Detects and creates the entire token hierarchy as a string."""
     token_hierarchy = [
         'token hierarchy',
         '===============',
@@ -36,23 +38,29 @@ def create_token_hierarchy() -> str:
     class_tuples: List[Tuple[str, object]] = [c for c in members if inspect.isclass(c[1])]
     classes = [c[1] for c in class_tuples]
     class_tree = inspect.getclasstree(classes)
-    create_token_subhierarchy(token_hierarchy, class_tree)
+    __create_token_subhierarchy(token_hierarchy, class_tree)
     token_hierarchy.append('')
 
     return '\n'.join(token_hierarchy)
 
 
-def create_token_subhierarchy(token_hierarchy: List[str], class_tree: list, indentation: str = '') -> List[str]:
+def __create_token_subhierarchy(
+        token_hierarchy: List[str],
+        class_tree: list,
+        indentation: str = '') -> None:
+    """Creates part of the token hierarchy.
+    
+    The result is returned by reference.
+    """
     for c in class_tree:
         if isinstance(c, list):
-            create_token_subhierarchy(token_hierarchy, c, indentation + '    ')
+            __create_token_subhierarchy(token_hierarchy, c, indentation + '    ')
         else:
             class_name = c[0].__name__
             if class_name not in ('object', 'ABC'):
                 abstract = ' (abstract)' if inspect.isabstract(c[0]) else ''
-                token_hierarchy.append(indentation[8:] + '* :py:class:`tokens.' + class_name + '`' + abstract)
-
-    return token_hierarchy
+                line = indentation[8:] + '* :py:class:`tokens.' + class_name + '`' + abstract
+                token_hierarchy.append(line)
 
 
 if __name__ == '__main__':
