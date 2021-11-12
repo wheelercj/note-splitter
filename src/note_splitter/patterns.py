8e72589e791ab4140654eaed411d22711c9db036
wheelercj <chrisw289@gmail.com>
2021-11-11 20:49:24 -0800

    Alphabetize the patterns

--- src/note_splitter/patterns.py
+++ src/note_splitter/patterns.py
@@ -10,32 +10,24 @@ Attributes
 ----------
 any_header : re.Pattern
     The pattern of a header of any level (a full-line element).
-horizontal_rule : re.Pattern
-    The pattern for a horizontal rule, which is composed of three or 
-    more minuses, underscores, or asterisks. There may be any number of 
-    whitespace characters anywhere on the line (a full-line element).
-frontmatter_fence : re.Pattern
-    The pattern for a frontmatter delimiter (a full-line element).
+blockquote : re.Pattern
+    The pattern for a quote that takes up one entire line (a full-line 
+    element).
 code_fence : re.Pattern
     The pattern for a multi-line code block delimiter (a full-line 
     element); the line starts with either three backticks or three 
     tildes.
+footnote : re.Pattern
+    The pattern for a footnote (a full-line element).
+frontmatter_fence : re.Pattern
+    The pattern for a frontmatter delimiter (a full-line element).
+horizontal_rule : re.Pattern
+    The pattern for a horizontal rule, which is composed of three or 
+    more minuses, underscores, or asterisks. There may be any number of 
+    whitespace characters anywhere on the line (a full-line element).
 math_fence : re.Pattern
     The pattern for the delimiter of a multi-line block of math 
     equations (a full-line element).
-blockquote : re.Pattern
-    The pattern for a quote that takes up one entire line (a full-line 
-    element).
-to_do : re.Pattern
-    The pattern for a to do list item that is not completed (a full-line
-    element).
-footnote : re.Pattern
-    The pattern for a footnote (a full-line element).
-unordered_list_item : re.Pattern
-    The pattern for an item in a bullet point list (a full-line 
-    element). The list can have bullet points as asterisks, minuses, or 
-    pluses. This pattern can also match some horizontal rules and to 
-    dos.
 ordered_list_item : re.Pattern
     The pattern for an item in an ordered list (a full-line element).
 table_divider : re.Pattern
@@ -46,6 +38,14 @@ table_row : re.Pattern
     The pattern for a row of a table (a full-line element). This pattern
     can also match some table dividers. Table rows that do not start and
     end with a pipe symbol are not supported.
+to_do : re.Pattern
+    The pattern for a to do list item that is not completed (a full-line
+    element).
+unordered_list_item : re.Pattern
+    The pattern for an item in a bullet point list (a full-line 
+    element). The list can have bullet points as asterisks, minuses, or 
+    pluses. This pattern can also match some horizontal rules and to 
+    dos.
 tag : re.Pattern
     The pattern for a tag (an inline element).
 """
@@ -57,19 +57,19 @@ tag : re.Pattern
 import re
 
 # full-line elements
-empty_line = re.compile(r'^\s*$')
 any_header = re.compile(r'^#+ .+')
-horizontal_rule = re.compile(r'^\s*(?:(?:-\s*){3,}|(?:\*\s*){3,}|(?:_\s*){3,})$')
-frontmatter_fence = re.compile(r'^(?:---|\*\*\*)$')
-code_fence = re.compile(r'^(?:```|~~~).*')
-math_fence = re.compile(r'^\s*\$\$\s*$')
 blockquote = re.compile(r'^(?:>\s*)+.+$')
-to_do = re.compile(r'^\s*[*+-] \[[x\s]\] .+')
+code_fence = re.compile(r'^(?:```|~~~).*')
+empty_line = re.compile(r'^\s*$')
 footnote = re.compile(r'^\[\^[^^\s\n][^^\n]*(?<!\s)\]: [^\n]+')
-unordered_list_item = re.compile(r'^\s*[*+-]\s.*')
+frontmatter_fence = re.compile(r'^(?:---|\*\*\*)$')
+horizontal_rule = re.compile(r'^\s*(?:(?:-\s*){3,}|(?:\*\s*){3,}|(?:_\s*){3,})$')
+math_fence = re.compile(r'^\s*\$\$\s*$')
 ordered_list_item = re.compile(r'^\s*\d+[.)]\s.*')
 table_divider = re.compile(r'^\|?(?: *[-:]{3,} *\|?)+$')
 table_row = re.compile(r'^\|.+\|$')
+to_do = re.compile(r'^\s*[*+-] \[[x\s]\] .+')
+unordered_list_item = re.compile(r'^\s*[*+-]\s.*')
 
 # inline elements
 tag = re.compile(r'(?<!\S)(?:(?:#+[\w\d_-]*[\w\d_-]#?)|(?:(?<=.)#+[\w\d_-]*[\w\d_-]#?))')
