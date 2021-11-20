b5feb4d364699674d0610ff535d4aaf3a4eda35b
sn6502 <91102256+sn6502@users.noreply.github.com>
2021-11-19 19:08:10 -0800

    added new settings

--- src/note_splitter/settings.py
+++ src/note_splitter/settings.py
@@ -61,6 +61,8 @@ note_types: List[str] = [".md", ".markdown", ".txt"]
 split_type: Type = tokens.Header
 split_attrs: dict = dict()
 new_file_name_format: str = r'%id'
+file_id_format: str = r'%Y%M%D%h%m%s'
+file_id_regex: str = r'\d{14}'
 create_blocks: bool = True
 copy_frontmatter: bool = True
 copy_global_tags: bool = True
@@ -73,11 +75,11 @@ def initialize_settings():
     connection = sqlite3.connect('store-transactions.db') 
     cur = connection.cursor()
     cur.execute('''CREATE TABLE settings (split_keyword text, source_folder_path text, destination_folder_path text,  
-                note_types text, split_type text, split_attrs text, new_file_name_format text,  create_blocks integer, 
+                note_types text, split_type text, split_attrs text, new_file_name_format text, file_id_format text, file_id_regex text, create_blocks integer, 
                 copy_frontmatter integer, copy_global_tags integer, backlink integer, create_index_file integer, replace_split_contents integer)''')
-    cur.execute("""INSERT INTO settings  (split_keyword, source_folder_path, destination_folder_path, note_types, split_type, split_attrs, 
-                new_file_name_format, create_blocks, copy_frontmatter, copy_global_tags, backlink, create_index_file, 
-                replace_split_contents) VALUES(?,?,?,?, ?, ?,?,?,?,?)""", (split_keyword, source_folder_path, destination_folder_path, 
+    cur.execute("""INSERT INTO settings  (split_keyword, source_folder_path, destination_folder_path,  
+                note_types, split_type, split_attrs, new_file_name_format, file_id_format, file_id_regex create_blocks, 
+                copy_frontmatter, copy_global_tags, backlink, create_index_file, replace_split_contents) VALUES(?,?,?,?, ?,?,?,?,?,?,?,?,?,?,?)""", (split_keyword, source_folder_path, destination_folder_path, 
                 ','.join(note_types), split_type__name__, json.dumps(split_attrs), new_file_name_format, int(create_blocks), int(copy_frontmatter), 
                 int(copy_global_tags), int(backlink), int(create_index_file), int(replace_split_contents)))
     connection.commit()
@@ -104,11 +106,11 @@ def update_settings():
     delete_current_settings()
     connection = sqlite3.connect('store-transactions.db') 
     cur = connection.cursor()
-    cur.execute("INSERT INTO settings  (split_keyword, source_folder_path, destination_folder_path, note_types, split_type, split_attrs, " 
-                "new_file_name_format, create_blocks, copy_frontmatter, copy_global_tags, backlink, create_index_file, replace_split_contents)" 
-                " VALUES(?,?,?,?, ?, ?,?,?,?,?)", (split_keyword, source_folder_path, destination_folder_path, ','.join(note_types), split_type__name__, 
-                json.dumps(split_attrs), new_file_name_format, int(create_blocks), int(copy_frontmatter), int(copy_global_tags), 
-                 int(backlink), int(create_index_file), int(replace_split_contents)))
+    cur.execute("""INSERT INTO settings  (split_keyword, source_folder_path, destination_folder_path,  
+                note_types, split_type, split_attrs, new_file_name_format, file_id_format, file_id_regex create_blocks, 
+                copy_frontmatter, copy_global_tags, backlink, create_index_file, replace_split_contents) VALUES(?,?,?,?, ?,?,?,?,?,?,?,?,?,?,?)""", (split_keyword, source_folder_path, destination_folder_path, 
+                ','.join(note_types), split_type__name__, json.dumps(split_attrs), new_file_name_format, int(create_blocks), int(copy_frontmatter), 
+                int(copy_global_tags), int(backlink), int(create_index_file), int(replace_split_contents)))
     connection.commit()
     connection.close()
 
