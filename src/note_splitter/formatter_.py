5a35f26997d456d6859f2bfbe5e70a8d89579367
wheelercj <chrisw289@gmail.com>
2021-12-11 23:56:46 -0800

    Split the formatter into multiple smaller functions, fix a bug

--- src/note_splitter/formatter_.py
+++ src/note_splitter/formatter_.py
@@ -8,7 +8,7 @@ strings.
 
 from typing import List, Optional
 import yaml  # https://pyyaml.org/wiki/PyYAMLDocumentation
-from note_splitter import settings, tokens
+from note_splitter import tokens
 
 
 class Formatter:
@@ -36,40 +36,86 @@ class Formatter:
             The frontmatter to add to each section.
         """
         split_contents: List[str] = []
-        if settings.split_type == tokens.Header:
-            check_for_level = False
-            difference_level = 0
-            for i, each in enumerate(sections):
-                # normalize the headers
-                each.content[0].content = each.content[0].content.replace('#', '')
-                if i == 0:
-                    if each.content[0].level > 1:
-                        difference_level = each.content[0].level - 1
-                        each.content[0].level -= difference_level
-                        check_for_level = True
-                else:
-                    if check_for_level:
-                        each.content[0].level -= difference_level
-                if each.content[0].level < 1:
-                    each.content[0].level = 1
-                each.content[0].content = '#' * each.content[0].level + each.content[0].content
+        for section in sections:
+            if not section.content:
+                continue
+            if isinstance(section.content[0], tokens.Header):
+                section_title = self.normalize_headers(section)
+            self.insert_global_tags(global_tags, section)
+            self.prepend_frontmatter(frontmatter, section_title, section)
+            split_contents.append(str(section))
+        return split_contents
 
+
+    def normalize_headers(self, section: tokens.Section) -> str:
+        """Normalizes the markdown header levels in a section.
+        
+        Parameters
+        ----------
+        section : tokens.Section
+            The section to normalize.
+
+        Returns
+        -------
+        str
+            The title of the section.
+        """
+        first_token = section.content[0]
+        if first_token.level <= 1:
+            return first_token.body
+        difference = first_token.level - 1
+        for i, token in enumerate(section.content):
+            if isinstance(token, tokens.Header):
+                section.content[i].level -= difference
+                section.content[i].content = \
+                    section.content[i].content[difference:]
+        return first_token.body
+
+
+    def insert_global_tags(self,
+                           global_tags: List[str], 
+                           section: tokens.Section) -> None:
+        """Inserts the global tags into a section.
+        
+        Parameters
+        ----------
+        global_tags : List[str]
+            The global tags to add to the section.
+        section : tokens.Section
+            The section to insert the global tags into.
+        """
+        i = 0
+        while i < len(section.content) \
+                and not isinstance(section.content[i], tokens.Header):
+            i += 1
+        i += 1
+        if i < len(section.content):
+            section.content.insert(i, tokens.Text(' '.join(global_tags)))
+        else:
+            section.content.insert(0, tokens.Text(' '.join(global_tags)))
+
+
+    def prepend_frontmatter(self,
+                            frontmatter: Optional[object],
+                            section_title: str,
+                            section: tokens.Section) -> None:
+        """Prepends the frontmatter to a section.
+        
+        Parameters
+        ----------
+        frontmatter : Optional[object]
+            The frontmatter to add to the section.
+        section_title : str
+            The title of the section.
+        section : tokens.Section
+            The section to prepend the frontmatter to.
+        """
+        if not frontmatter:
+            return
         if isinstance(frontmatter, dict):
             if 'title' in frontmatter:
-                frontmatter['new_name'] = frontmatter.pop('title')
+                frontmatter['title'] = section_title
         frontmatter_string = yaml.dump(frontmatter)
-        
-        # insert the frontmatter and global tags into each section
-        if frontmatter:
-            for i in range(len(sections)):
-                sections[i].content = ['---\n' + frontmatter_string + '---\n'] + global_tags + sections[i].content
-        else:
-            for i in range(len(sections)):
-                if sections[i].content[0] == tokens.Header:
-                    sections[i].content = [sections[i].content[0]] + global_tags + sections[i].content[1:]
-                else:
-                    sections[i].content = global_tags + sections[i].content
-
-        for each in sections:
-            split_contents.append(str(each))
-        return split_contents
+        frontmatter_string = '---\n' + frontmatter_string + '---\n'
+        frontmatter_string = frontmatter_string.replace('\n\n', '\n')
+        section.content.insert(0, tokens.Text(frontmatter_string))
