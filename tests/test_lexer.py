a6fa480c7c3246a781e11b5bcfdd4fa76154332f
wheelercj <chrisw289@gmail.com>
2022-01-03 18:10:33 -0800

    Add tests for lexer.py

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -1,2 +1,41 @@
-from note_splitter.lexer import Lexer
+from note_splitter import tokens, lexer
 
+
+def test_tokenize():
+    line = '# This is a header'
+    tokenize = lexer.Lexer()
+    token = tokenize(line)[0]
+    assert isinstance(token, tokens.Header)
+
+
+def test_tokenize_with_text():
+    line = 'This is text'
+    tokenize = lexer.Lexer()
+    token = tokenize(line)[0]
+    assert isinstance(token, tokens.Text)
+
+
+def test_tokenize_with_text_and_header():
+    line = '# This is a header\nThis is text'
+    tokenize = lexer.Lexer()
+    tokens_ = tokenize(line)
+    assert len(tokens_) == 2
+    assert isinstance(tokens_[0], tokens.Header)
+    assert isinstance(tokens_[1], tokens.Text)
+
+
+def test_tokenize_with_tag_in_footnote():
+    line = '[^1]: This is a footnote with a #tag'
+    tokenize = lexer.Lexer()
+    tokens_ = tokenize(line)
+    assert isinstance(tokens_[0], tokens.Footnote)
+
+
+def test_tokenize_with_code_block():
+    content = '```\nThis is a code block\n```'
+    tokenize = lexer.Lexer()
+    tokens_ = tokenize(content)
+    assert len(tokens_) == 3
+    assert isinstance(tokens_[0], tokens.CodeFence)
+    assert isinstance(tokens_[1], tokens.Code)
+    assert isinstance(tokens_[2], tokens.CodeFence)
